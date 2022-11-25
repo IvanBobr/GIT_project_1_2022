a5b0543e71b0b2639769c26999c5b0c29209b689
Root_ <ivanbobr365@gmail.com>
2022-11-25 18:04:19 +0300

    ADDED commands for buttons

--- Lib/design.py
+++ Lib/design.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtWidgets import QLabel, QLineEdit, QTextBrowser
 from PyQt5.QtGui import QFont
 from datetime import datetime
+import docx
 
 
 class Example(QWidget):
@@ -12,7 +13,7 @@ class Example(QWidget):
         self.orient_left = True
 
     def initUI(self):
-        self.setGeometry(0, 0, 750, 850)
+        self.setGeometry(0, 0, 784, 850)
         self.setWindowTitle('Изменения в расписании')
 
         self.main_title = QLabel(self)
@@ -21,11 +22,12 @@ class Example(QWidget):
         self.main_title.setFont(QFont("Franklin Gothic Demi", 18))
 
         self.textBrowse = QTextBrowser(self)
-        self.textBrowse.setGeometry(180, 41, 561, 751)
+        self.textBrowse.setGeometry(180, 41, 601, 751)
+        self.textBrowse.setFont(QFont("Franklin Gothic Demi", 14))
 
         self.button_goMain = QPushButton(self)
         self.button_goMain.setText("На главную")
-        self.button_goMain.setGeometry(582, 10, 161, 28)
+        self.button_goMain.setGeometry(620, 10, 161, 28)
 
         self.label_time = QLabel(self)
         self.label_time.setText(datetime.now().strftime("%H:%M"))
@@ -34,8 +36,8 @@ class Example(QWidget):
 
         push_buttons(self)
 
-    def run(self):
-        self.label_time.setText(datetime.now().strftime("%H:%M"))
+    def run(self, key_sort):
+        self.textBrowse.setText(read_from_word_doc(key_sort))
 
 
 def push_buttons(self):
@@ -87,7 +89,29 @@ def push_buttons(self):
     self.button_10.setFont(QFont("MS Shell Dlg 2", 10))
     self.button_11.setFont(QFont("MS Shell Dlg 2", 10))
 
-    # self.button_.clicked.connect(lambda x: )
+    self.button_1.clicked.connect(lambda x: self.run("1-"))
+    self.button_2.clicked.connect(lambda x: self.run("2-"))
+    self.button_3.clicked.connect(lambda x: self.run("3-"))
+    self.button_4.clicked.connect(lambda x: self.run("4-"))
+    self.button_5.clicked.connect(lambda x: self.run("5-"))
+    self.button_6.clicked.connect(lambda x: self.run("6-"))
+    self.button_7.clicked.connect(lambda x: self.run("7-"))
+    self.button_8.clicked.connect(lambda x: self.run("8-"))
+    self.button_9.clicked.connect(lambda x: self.run("9-"))
+    self.button_10.clicked.connect(lambda x: self.run("10-"))
+    self.button_11.clicked.connect(lambda x: self.run("11-"))
+
+
+def read_from_word_doc(key_sort, name="IZM.docx"):
+    document = docx.Document(name)
+    string_return = ""
+    for par in document.paragraphs:
+        if key_sort in par.text:
+            for_new_str = par.text.split()
+            new_string = for_new_str[0] + " " + for_new_str[1] + "  " + for_new_str[2] + "   " + " ".join(
+                for_new_str[3:])
+            string_return += new_string + "\n\n"
+    return string_return
 
 
 if __name__ == '__main__':
